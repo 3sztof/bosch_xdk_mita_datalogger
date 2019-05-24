bcb2aac3dd00700d55a9bfc1fcb44830acebb4a0
Krzysztof Wilczynski <3sztof@pm.me>
2019-05-24 10:12:00 +0200

    Working again: fixed permanent cycle number (SD binary log), code cleanup, added cycle time and pressure sensor reading.

---
+++ src-gen/debug/objects/application.d
@@ -0,0 +1,110 @@
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/debug/objects/application.o: \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/application.c \
+ ../Common/legacy/include/XdkSensorHandle.h \
+ ../Common/include/Sensor/XDK_SensorHandle.h \
+ ../Platform/Sensors/include/BCDS_Accelerometer.h \
+ ../Platform/Essentials/include/BCDS_Retcode.h \
+ ../Platform/Essentials/include/BCDS_Basics.h \
+ ../Platform/Sensors/include/BCDS_Gyroscope.h \
+ ../Platform/Sensors/include/BCDS_LightSensor.h \
+ ../Platform/Sensors/include/BCDS_Magnetometer.h \
+ ../Platform/Sensors/include/BCDS_Environmental.h \
+ ../Platform/SensorToolbox/include/BCDS_Orientation.h \
+ ../Platform/SensorToolbox/include/BCDS_Rotation.h \
+ ../Platform/SensorToolbox/include/BCDS_Gravity.h \
+ ../Platform/SensorToolbox/include/BCDS_CalibratedAccel.h \
+ ../Platform/SensorToolbox/include/BCDS_AbsoluteHumidity.h \
+ ../Platform/SensorToolbox/include/BCDS_StepCounter.h \
+ ../Platform/SensorToolbox/include/BCDS_CalibratedAccel.h \
+ ../Platform/SensorToolbox/include/BCDS_CalibratedGyro.h \
+ ../Platform/SensorToolbox/include/BCDS_CalibratedMag.h \
+ ../Platform/SensorToolbox/include/BCDS_FingerPrint.h \
+ ../Platform/SensorToolbox/include/BCDS_LinearAcceleration.h \
+ ../Platform/SensorToolbox/include/BCDS_Gestures.h \
+ C:\XDK-Workbench360\XDK\SDK/xdk110/Libraries/BSTLib/3rd-party/bstlib/BMA2x2_driver/bma2x2.h \
+ ../Platform/Essentials/include/BCDS_Basics.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorEnvironment.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorAccelerometer.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/PlatformXDK110.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/xdk110Types.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/xdk110Types.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/InputOutputSDCardSd.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaGeneratedTypes.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/applicationTypes.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/ConnectivityLEDLed.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaExceptions.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaGeneratedTypes.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/application.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/applicationTypes.h
+
+../Common/legacy/include/XdkSensorHandle.h:
+
+../Common/include/Sensor/XDK_SensorHandle.h:
+
+../Platform/Sensors/include/BCDS_Accelerometer.h:
+
+../Platform/Essentials/include/BCDS_Retcode.h:
+
+../Platform/Essentials/include/BCDS_Basics.h:
+
+../Platform/Sensors/include/BCDS_Gyroscope.h:
+
+../Platform/Sensors/include/BCDS_LightSensor.h:
+
+../Platform/Sensors/include/BCDS_Magnetometer.h:
+
+../Platform/Sensors/include/BCDS_Environmental.h:
+
+../Platform/SensorToolbox/include/BCDS_Orientation.h:
+
+../Platform/SensorToolbox/include/BCDS_Rotation.h:
+
+../Platform/SensorToolbox/include/BCDS_Gravity.h:
+
+../Platform/SensorToolbox/include/BCDS_CalibratedAccel.h:
+
+../Platform/SensorToolbox/include/BCDS_AbsoluteHumidity.h:
+
+../Platform/SensorToolbox/include/BCDS_StepCounter.h:
+
+../Platform/SensorToolbox/include/BCDS_CalibratedAccel.h:
+
+../Platform/SensorToolbox/include/BCDS_CalibratedGyro.h:
+
+../Platform/SensorToolbox/include/BCDS_CalibratedMag.h:
+
+../Platform/SensorToolbox/include/BCDS_FingerPrint.h:
+
+../Platform/SensorToolbox/include/BCDS_LinearAcceleration.h:
+
+../Platform/SensorToolbox/include/BCDS_Gestures.h:
+
+C:\XDK-Workbench360\XDK\SDK/xdk110/Libraries/BSTLib/3rd-party/bstlib/BMA2x2_driver/bma2x2.h:
+
+../Platform/Essentials/include/BCDS_Basics.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorEnvironment.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorAccelerometer.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/PlatformXDK110.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/xdk110Types.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/xdk110Types.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/InputOutputSDCardSd.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaGeneratedTypes.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/applicationTypes.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/ConnectivityLEDLed.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaExceptions.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaGeneratedTypes.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/application.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/applicationTypes.h:
---
+++ src-gen/debug/objects/main.d
@@ -0,0 +1,83 @@
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/debug/objects/main.o: \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/main.c \
+ ../Platform/Essentials/include/BCDS_Basics.h \
+ ../Platform/BSP/include/BSP_ExtensionPort.h \
+ ../Platform/Essentials/include/BCDS_HAL.h \
+ ../Common/config/Essentials/BCDS_HALConfig.h \
+ ../Platform/Essentials/include/bsp/BCDS_BSP_Board.h \
+ ../Platform/Essentials/include/BCDS_Retcode.h \
+ ../Platform/Utils/include/BCDS_CmdProcessor.h \
+ ../Platform/Utils/include/BCDS_Utils.h \
+ ../Common/config/Utils/BCDS_UtilsConfig.h \
+ C:\XDK-Workbench360\XDK\SDK/xdk110/Common/include/XdkSystemStartup.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/application.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/applicationTypes.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorButton_two.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorButton_one.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorEnvironment.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorAccelerometer.h \
+ C:\XDK-Workbench360\XDK\SDK/xdk110/Libraries/BSTLib/3rd-party/bstlib/BMA2x2_driver/bma2x2.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/PlatformXDK110.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/xdk110Types.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/xdk110Types.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/ConnectivityLEDLed.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/InputOutputSDCardSd.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaGeneratedTypes.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/applicationTypes.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaTime.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaEvents.h \
+ C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaExceptions.h
+
+../Platform/Essentials/include/BCDS_Basics.h:
+
+../Platform/BSP/include/BSP_ExtensionPort.h:
+
+../Platform/Essentials/include/BCDS_HAL.h:
+
+../Common/config/Essentials/BCDS_HALConfig.h:
+
+../Platform/Essentials/include/bsp/BCDS_BSP_Board.h:
+
+../Platform/Essentials/include/BCDS_Retcode.h:
+
+../Platform/Utils/include/BCDS_CmdProcessor.h:
+
+../Platform/Utils/include/BCDS_Utils.h:
+
+../Common/config/Utils/BCDS_UtilsConfig.h:
+
+C:\XDK-Workbench360\XDK\SDK/xdk110/Common/include/XdkSystemStartup.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/application.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/applicationTypes.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorButton_two.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorButton_one.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorEnvironment.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/SensorAccelerometer.h:
+
+C:\XDK-Workbench360\XDK\SDK/xdk110/Libraries/BSTLib/3rd-party/bstlib/BMA2x2_driver/bma2x2.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/PlatformXDK110.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/xdk110Types.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/xdk110Types.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/ConnectivityLEDLed.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/InputOutputSDCardSd.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaGeneratedTypes.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/applicationTypes.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaTime.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaEvents.h:
+
+C:/Users/cwk1wz1/XDK-Workspace/DataLogger/src-gen/base/MitaExceptions.h:
